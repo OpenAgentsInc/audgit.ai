9f2fdad166db5b5d8f7820083d27587c6af31915
Christopher David <chris@arcadelabs.co>
2023-07-29 13:15:57 -0700

    and back button

--- pages/audit.tsx
+++ pages/audit.tsx
@@ -1,14 +1,21 @@
+import { LinkButton } from "../primitives/Button"
+
 export default function Audit() {
   return (
     <div
       style={{
-        backgroundColor: "black",
+        backgroundColor: "#121215",
         height: "100vh",
         width: "100vw",
         padding: 40,
       }}
     >
-      <h1>AUDIT A GITHUB ISSUE</h1>
+      <div style={{ maxWidth: 70 }}>
+        <LinkButton href="/" variant="secondary">
+          Home
+        </LinkButton>
+      </div>
+      <h1 style={{ marginTop: 30 }}>AUDIT A GITHUB ISSUE</h1>
       <input
         placeholder="Enter a GitHub issue URL"
         defaultValue={"https://github.com/ArcadeLabsInc/arcade/issues/447"}
