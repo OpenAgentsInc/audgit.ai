4affb323600152c4a6ef955bd3c600421f10a213
Christopher David <chris@arcadelabs.co>
2023-07-29 16:07:35 -0700

    awesome

---
+++ components/EventFeed.tsx
@@ -0,0 +1,47 @@
+export const EventFeed = ({ eventFeed }) => {
+  return (
+    <div style={{ marginTop: 30, maxWidth: 660 }}>
+      {eventFeed.map((event) => {
+        let date = "unknown";
+
+        if (event.created_at) {
+          const timeOptions = {
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+          };
+
+          date = new Date(event.created_at * 1000).toLocaleString(
+            "en-US",
+            // @ts-ignore
+            timeOptions
+          );
+        }
+
+        return (
+          <div
+            key={event.id}
+            style={{
+              backgroundColor: "#444",
+              padding: 10,
+              borderRadius: 8,
+              marginTop: 10,
+            }}
+          >
+            <p
+              style={{
+                textAlign: "right",
+                marginBottom: 2,
+                color: "#999",
+                fontSize: 14,
+              }}
+            >
+              {date}
+            </p>
+            <p>{event.content}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- pages/audit.tsx
+++ pages/audit.tsx
@@ -3,6 +3,7 @@ import NDK, {
     NDKEvent, NDKPrivateKeySigner, NDKSubscription
 } from "@nostr-dev-kit/ndk"
 import { DocumentRowSkeleton } from "../components/Documents"
+import { EventFeed } from "../components/EventFeed"
 import { PlusIcon } from "../icons"
 import { MarketingLayout } from "../layouts/Marketing"
 import { RELAYS, useNDK } from "../lib/client/hooks/state"
@@ -114,58 +115,12 @@ export default function Audit() {
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
           />
-          <Button
-            // icon={<PlusIcon />}
-            type="submit"
-            style={{ padding: 24, borderRadius: 8 }}
-          >
+          <Button type="submit" style={{ padding: 24, borderRadius: 8 }}>
             Submit
           </Button>
         </form>
 
-        <div style={{ marginTop: 30, maxWidth: 660 }}>
-          {eventFeed.map((event) => {
-            let date = "unknown";
-
-            if (event.created_at) {
-              const timeOptions = {
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric",
-              };
-
-              date = new Date(event.created_at * 1000).toLocaleString(
-                "en-US",
-                // @ts-ignore
-                timeOptions
-              );
-            }
-
-            return (
-              <div
-                key={event.id}
-                style={{
-                  backgroundColor: "#444",
-                  padding: 10,
-                  borderRadius: 8,
-                  marginTop: 10,
-                }}
-              >
-                <p
-                  style={{
-                    textAlign: "right",
-                    marginBottom: 2,
-                    color: "#999",
-                    fontSize: 14,
-                  }}
-                >
-                  {date}
-                </p>
-                <p>{event.content}</p>
-              </div>
-            );
-          })}
-        </div>
+        <EventFeed eventFeed={eventFeed} />
       </div>
     </MarketingLayout>
   );
